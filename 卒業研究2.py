20c509b7a40d88563a756db35dcdc6bd0b7483a4
KSumiyama <kou0616s@gmail.com>
2025-12-02 15:11:13 +0900

    Update 卒業研究2.py

--- 卒業研究2.py
+++ 卒業研究2.py
@@ -66,7 +66,7 @@ if uploaded_file is not None:
       
     st.subheader("アップロードされたデータ")
     st.write("出欠：出場…１、欠場…０")
-    st.write("守指定（守らせたいポジション）：１～９…ピッチャー～ライト")
+    st.write("守指定（その選手に守らせたいポジション）：１～９（ピッチャー～ライトの守備番号）")
     st.write("打撃力、投～右（ピッチャー～ライト）：その選手の打撃力、守備ポジションの技量を数値化")
 
     df = df.dropna(subset=[df.columns[1]])
