4d24d6aaf5fb06aba5459ca3f1489a95a4d6e9c0
KSumiyama <kou0616s@gmail.com>
2026-01-12 13:25:25 +0900

    Update 卒業研究2.py

--- 卒業研究2.py
+++ 卒業研究2.py
@@ -355,7 +355,7 @@ if uploaded_file is not None:
             #df_result = df_result.drop(index=0).reset_index(drop=True)
             df_result = df_result.drop(columns=df_result.columns[0])
             
-            st.subheader("最適化結果（チーム分け結果）")
+            st.subheader("チーム分け結果")
 
             def is_number(x):
                 try:
@@ -436,7 +436,7 @@ if uploaded_file is not None:
             df_result = df_result.drop(columns=df_result.columns[0])
             
             
-            st.subheader("最適化結果（チーム分け結果）")
+            st.subheader("チーム分け結果")
             def is_number(x):
                 try:
                     float(x)
@@ -577,7 +577,7 @@ if uploaded_file is not None:
             #df_result = df_result.drop(index=0).reset_index(drop=True)
             df_result = df_result.drop(columns=df_result.columns[0])
             
-            st.subheader("最適化結果（チーム分け結果）")
+            st.subheader("チーム分け結果")
             def is_number(x):
                 try:
                     float(x)
@@ -655,7 +655,7 @@ if uploaded_file is not None:
             #df_result = df_result.drop(index=0).reset_index(drop=True)
             df_result = df_result.drop(columns=df_result.columns[0])
             
-            st.subheader("最適化結果（チーム分け結果）")
+            st.subheader("チーム分け結果")
             def is_number(x):
                 try:
                     float(x)
